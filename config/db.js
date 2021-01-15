ad7075b2216581243ec96a2e82b91980c732e1ea
unknown <tony464us2011@yahoo.com>
2021-01-14 23:05:43 -0500

    setup backend

---
+++ config/db.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+import config from 'config'
+const db = config.get('MONGO_URI');
+
+const connectDB = async () => {
+
+    try {
+       await mongoose.connect(db, {
+            useNewUrlParser: true,
+            useCreateIndex: true,
+            useFindAndModify: false,
+            useUnifiedTopology: true
+        });
+        console.log('MongoDB Connected')
+    } catch(err) {
+        console.error(err.message);
+        process.exit(1)
+    }
+}
+
+export default connectDB
---
+++ server.js
@@ -0,0 +1,50 @@
+import express from 'express'
+import dotenv from 'dotenv'
+import morgan from 'morgan'
+import cors from 'cors'
+import path from 'path'
+import connectDB from './config/db.js'
+import productRoutes from './routes/productRoutes.js'
+import userRoutes from './routes/userRoutes.js'
+import orderRoutes from './routes/orderRoutes.js'
+import uploadRoutes from './routes/uploadRoutes.js'
+
+const app = express()
+dotenv.config()
+
+//Connect to DB
+connectDB();
+
+if(process.env.NODE_ENV === 'development') {
+    app.use(morgan('dev'))
+}
+
+app.use(cors())
+app.use(express.urlencoded({ limit: '50mb', extended: false }));
+app.use(express.json({limit: '50mb'}));
+
+
+app.use('/api/products', productRoutes)
+app.use('/api/users', userRoutes)
+app.use('/api/orders', orderRoutes)
+app.use('/api/upload', uploadRoutes)
+
+app.get('/order/api/config/paypal', (req, res) => res.send(process.env.PAYPAL_CLIENT_ID))
+
+const __dirname = path.resolve()
+
+app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
+
+
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api is running')
+    })
+}
+
+const PORT = process.env.PORT || 5000
+
+app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`))
