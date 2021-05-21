fcd3112f66527d3fe4feacc4dd658758f0792a34
unknown <tony464us2011@yahoo.com>
2021-05-20 22:40:54 -0400

    prepare for deployment

--- server.js
+++ server.js
@@ -40,14 +40,14 @@ const __dirname = path.resolve()
 app.use('/uploads', express.static(path.join(__dirname, '/uploads')))
 
 
-// if (process.env.NODE_ENV === 'p') {
-//     app.use(express.static(path.join(__dirname, '/frontend/build')));
-//     app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
-// } else {
-//     app.get('/', (req, res) => {
-//         res.send('Api is running')
-//     })
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(express.static(path.join(__dirname, '/frontend/build')));
+    app.get('*', (req, res) => res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html')))
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api is running')
+    })
+}
 
 const PORT = process.env.PORT || 5000
 
