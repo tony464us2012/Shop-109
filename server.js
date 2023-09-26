c11742e91d85b9aef9cf540ac78c0ede226c2157
Anthony <tony464us2011@yahoo.com>
2023-09-25 22:58:36 -0400

    deployment

--- server.js
+++ server.js
@@ -19,9 +19,9 @@ connectDB();
 
 app.use(cookieParser())
 
-// app.enable('trust proxy');
+app.enable('trust proxy');
 
-// app.use(cors())
+app.use(cors())
 app.use(express.urlencoded({ limit: '50mb', extended: false }));
 app.use(express.json({ limit: '50mb' }));
 // app.use((req, res, next) => {
